01952ea80fee09f12b8be627db27b88f3a57f70e
khalid-nur <khalidnur150@gmail.com>
2026-02-20 16:22:24 -0600

    initial commit

---
+++ src/main/java/com/tubenotify/tubenotify_backend/TubenotifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tubenotify.tubenotify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubenotifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubenotifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tubenotify/tubenotify_backend/TubenotifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubenotify.tubenotify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubenotifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
